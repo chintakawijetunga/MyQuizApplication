d83dc0ae86a901587b66cdf96e19902d5345537b
Chintaka Wijetunga <chintaka.wijetunga@yahoo.com>
2017-08-06 13:40:17 +0530

    Login Logic Issues

--- app/src/main/java/com/example/chwtlk/myquizapplication/LoginMenu.java
+++ app/src/main/java/com/example/chwtlk/myquizapplication/LoginMenu.java
@@ -28,14 +28,19 @@ public class LoginMenu extends AppCompatActivity{
         txtUserName = (TextView) findViewById(R.id.txtUserName);
         txtPassword = (EditText) findViewById(R.id.txtPasswordBox);
         errorMessage = new AlertDialog.Builder(this);
+
+
+
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(txtUserName.getText() == "admin" && txtPassword.getText().toString() == "admin")
-                    startActivity(new Intent(getApplicationContext(),MainMenu.class));
+                if(txtUserName.getText().toString().equals("admin") && txtPassword.getText().toString().equals("admin")) {
+                    startActivity(new Intent(getApplicationContext(), MainMenu.class));
+                }
                 else {
                     errorMessage.setTitle("Error");
                     errorMessage.setMessage("Invalid user name or password!");
+                    errorMessage.show();
                 }
             }
         });
