a1ed0c1afc51863d905e6c194bb62e46eed85464
disni charuka <disnicharuka@gmail.com>
2017-08-06 13:25:25 +0530

    Login Logic

--- app/src/main/java/com/example/chwtlk/myquizapplication/LoginMenu.java
+++ app/src/main/java/com/example/chwtlk/myquizapplication/LoginMenu.java
@@ -1,34 +1,44 @@
 package com.example.chwtlk.myquizapplication;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginMenu extends AppCompatActivity{
     Button buttonLogin;
+    TextView txtUserName;
+    EditText txtPassword;
+    AlertDialog.Builder errorMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_menu);
 
-        buttonLogin = (Button) findViewById(R.id.buttonLogin);
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainMenu.class));
-            }
-        });
+        login();
+    }
 
-        buttonLogin = (Button) findViewById(R.id.buttonSubMenu);
+    private void login()
+    {
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        txtUserName = (TextView) findViewById(R.id.txtUserName);
+        txtPassword = (EditText) findViewById(R.id.txtPasswordBox);
+        errorMessage = new AlertDialog.Builder(this);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainSubMenu.class));
+                if(txtUserName.getText() == "admin" && txtPassword.getText().toString() == "admin")
+                    startActivity(new Intent(getApplicationContext(),MainMenu.class));
+                else {
+                    errorMessage.setTitle("Error");
+                    errorMessage.setMessage("Invalid user name or password!");
+                }
             }
         });
     }
-
 
 }
