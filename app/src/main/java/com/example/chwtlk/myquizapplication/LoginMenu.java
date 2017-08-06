0e057d9378022311405ac85a5dc34dca3c03bce9
Chintaka Wijetunga <chintaka.wijetunga@yahoo.com>
2017-08-06 11:59:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chwtlk/myquizapplication/LoginMenu.java
@@ -0,0 +1,30 @@
+package com.example.chwtlk.myquizapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginMenu extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_menu);
+
+        Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(),MainMenu.class));
+
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/chwtlk/myquizapplication/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.chwtlk.myquizapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread splash_Screen = new Thread(){
+
+            public void run(){
+
+                try{
+
+                    sleep(5000);
+
+                }catch(Exception e){
+
+                    e.printStackTrace();
+
+                }finally{
+                    startActivity(new Intent(getApplicationContext(),LoginMenu.class));
+
+                    finish();
+                }
+            }
+        };
+
+        splash_Screen.start();
+    }
+}
