7fc5b41e3ffc1240ce93519f863ca59c68b72dd0
Disni Charuka <disnicharuka@gmail.com>
2017-09-09 22:26:22 +0530

    Quiz1 class created

--- app/src/main/java/com/example/chwtlk/myquizapplication/MainMenu.java
+++ app/src/main/java/com/example/chwtlk/myquizapplication/MainMenu.java
@@ -1,13 +1,30 @@
 package com.example.chwtlk.myquizapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
-
+    Button buttonPlayGame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        buttonClick();
+    }
+
+    private void buttonClick() {
+        buttonPlayGame = (Button) findViewById(R.id.btnPlayGame);
+
+        buttonPlayGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Quiz1.class));
+            }
+        });
+
     }
 }
