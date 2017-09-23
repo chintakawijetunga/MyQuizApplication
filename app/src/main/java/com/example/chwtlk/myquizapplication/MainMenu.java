b1b42bcd5f57f1420964d2a2a3995f34e5033f56
Chintaka Wijetunga <chintaka.wijetunga@yahoo.com>
2017-09-23 11:28:19 +0530

    Resource file using corrected

--- app/src/main/java/com/example/chwtlk/myquizapplication/MainMenu.java
+++ app/src/main/java/com/example/chwtlk/myquizapplication/MainMenu.java
@@ -16,7 +16,7 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        Log.d("lifecycle", String.valueOf(R.string.on_create));
+        Log.d("lifecycle", getResources().getString(R.string.on_create));
         buttonClick();
     }
 
