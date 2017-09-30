4c8399d72705542e857a32c1621d78a15089a812
Chintaka Wijetunga <chintaka.wijetunga@yahoo.com>
2017-09-30 11:34:45 +0530

    Take a video

---
+++ app/src/main/java/com/example/chwtlk/myquizapplication/VideoActivity.java
@@ -0,0 +1,36 @@
+package com.example.chwtlk.myquizapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class VideoActivity extends AppCompatActivity {
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        dispatchTakeVideoIntent();
+    }
+
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = intent.getData();
+            VideoView mVideoView =(VideoView)findViewById(R.id.mVideoView);
+            mVideoView.setVideoURI(videoUri);
+        }
+    }
+}
